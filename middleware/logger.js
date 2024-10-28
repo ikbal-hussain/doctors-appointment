40bfec70dc4e4e256d155d899556873a7c667656
Ikbal Hussain <ikbalhussain3674@gmail.com>
2024-10-28 16:05:29 +0530

    files added

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,9 +1,4 @@
-const moment = require("moment");
-
-const logger = (req, res, next) => {
-  console.log(`[${moment().format()}] ${req.method} ${req.url}`);
-  next();
+module.exports = (req, res, next) => {
+    console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
+    next();
 };
-
-module.exports = logger;
-
--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-const express = require("express");
-const moment = require("moment");
-const logger = require("./middleware/logger");
-const doctorRoutes = require("./routes/doctors");
-const patientRoutes = require("./routes/patients");
-const appointmentRoutes = require("./routes/appointments");
+const express = require('express');
+const mongoConfig = require('./config/mongoConfig');
+const { sequelize, testConnection } = require('./config/sqlConfig');
+const authRoutes = require('./routes/authRoutes');
+const doctorRoutes = require('./routes/doctorRoutes');
+const patientRoutes = require('./routes/patientRoutes');
+const appointmentRoutes = require('./routes/appointments');
 
 const app = express();
-const PORT = 4007;
-
 app.use(express.json());
-app.use(logger);
 
-// Routes
-app.use("/doctors", doctorRoutes);
-app.use("/patients", patientRoutes);
-app.use("/appointments", appointmentRoutes);
+mongoConfig();
+testConnection(); // This is to check MySQL connection
+
+app.use('/auth', authRoutes);
+app.use('/doctors', doctorRoutes);
+app.use('/patients', patientRoutes);
+app.use('/appointments', appointmentRoutes);
+
+const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
-
